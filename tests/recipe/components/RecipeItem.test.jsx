f765a35c9eecb00f87a900bd892c1add761afd72
maximiliano.chia <maximiliano.chia@accenture.com>
2023-07-07 11:32:26 -0300

    test updated

--- tests/recipe/components/RecipeItem.test.jsx
+++ tests/recipe/components/RecipeItem.test.jsx
@@ -1,7 +1,16 @@
 /* eslint-disable no-undef */
 import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
 import { RecipeItem } from '../../../src/recipe/components/RecipeItem';
 
+
+const mockedUseContext = jest.fn();
+
+jest.mock('react', () => ({
+    ...jest.requireActual('react'),
+    useContext: () => mockedUseContext
+}));
 
 describe('Pruebas en <RecipeItem />', () => {
 
@@ -10,7 +19,7 @@ describe('Pruebas en <RecipeItem />', () => {
         imagePath: 'https://www.gourmet.cl/wp-content/uploads/2022/05/Pollo-al-Horno-Web-570x458.jpg',
         name: 'pollo',
         description: 'receta pollo',
-    }
+    };
 
     test('debe hacer match con el snapshot', () => {
         const { container } = render(<RecipeItem recipeItems={ recipe } onDeleteRecipe={() => {}} />);
